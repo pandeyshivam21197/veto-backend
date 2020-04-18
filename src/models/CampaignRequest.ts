baf881e892444fff9af759f1c4a190eda7d1af02
Shivam <shivam.pandey@nineleaps.com>
2020-04-18 17:07:25 +0530

    added Campaign request model updated

--- src/models/CampaignRequest.ts
+++ src/models/CampaignRequest.ts
@@ -2,21 +2,28 @@ import {model, Schema, Document} from 'mongoose';
 
 interface IEntity {
     title: string,
-    subTitle?: string,
     requestedAmount: number,
     availedAmount?: number,
 }
 export type CampaignRequestModel = Document & {
-    userId: Schema.Types.ObjectId;
+    creatorId: Schema.Types.ObjectId;
+    supporterIds?: [Schema.Types.ObjectId];
     title: string;
     subTitle?: string;
     entities?: [IEntity];
+    status?: string,
 };
 
 const campaignRequestSchema: Schema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
+    creatorId: {
+        type: Schema.Types.ObjectId, // Using token userId is extracted
         ref: 'User',
+        required: true,
+    },
+    supporterIds: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User',
+        default: [],
     },
     title: {
         type: String,
@@ -33,10 +40,6 @@ const campaignRequestSchema: Schema = new Schema({
                     type: String,
                     required: true,
                 },
-                subTitle: {
-                    type: String,
-                    default: '',
-                },
                 requestedAmount: {
                     type: Number,
                     required: true,
@@ -48,6 +51,10 @@ const campaignRequestSchema: Schema = new Schema({
             },
         ],
         default: [],
+    },
+    status: {
+        type: String,
+        default: '',
     },
 }, {timestamps: true});
 
